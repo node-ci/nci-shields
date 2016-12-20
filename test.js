03b9e4a688875de23f8c21251ee120cade11a284
fleg <flegmatik.bel@gmail.com>
2016-12-20 22:58:39 +0300

    add route to get shield by build id close #1

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ var _ = require('underscore'),
 	sinon = require('sinon'),
 	plugin = require('./lib');
 
-describe('Shields', function() {
-	var handler;
+describe('Shields plugin', function() {
+	var projectHandler, buildHandler;
 
 	var app = {
 		lib: {
@@ -17,8 +17,17 @@ describe('Shields', function() {
 		projects: {},
 		builds: {},
 		express: {
-			get: function(route, _handler) {
-				handler = _handler;
+			get: function(route, handler) {
+				switch (route) {
+					case '/shields/:projectName.svg':
+						projectHandler = handler;
+						break;
+					case '/shields/builds/:id(\d+).svg':
+						buildHandler = handler;
+						break;
+					default:
+						break;
+				}
 			}
 		}
 	};
@@ -41,6 +50,12 @@ describe('Shields', function() {
 		expect(res.end.firstCall.args[0]).match(new RegExp(text));
 	};
 
+	var statusesHash = {
+		done: 'passing',
+		'in-progress': 'in progress',
+		error: 'failing'
+	};
+
 	before(function() {
 		plugin.register(app);
 	});
@@ -51,49 +66,80 @@ describe('Shields', function() {
 		res.end.reset();
 	});
 
-	it('should be ok with unexisted project', function() {
-		app.projects.get = sinon.stub().returns(null);
-		handler(getReq({projectName: 'unexisted'}), res);
+	describe('project', function() {
+		it('should be ok with unexisting project', function() {
+			app.projects.get = sinon.stub().returns(null);
+			projectHandler(getReq({projectName: 'unexisted'}), res);
 
-		expect(res.sendStatus.firstCall.args[0]).to.eql(404);
-	});
+			expect(res.sendStatus.firstCall.args[0]).to.eql(404);
+		});
 
-	it('should be ok with project without builds', function() {
-		app.projects.get = sinon.stub().returns({});
-		app.builds.getRecent = sinon.stub().callsArgWith(1, null, []);
-		handler(getReq({projectName: 'ok'}), res);
-
-		checkSvg('unknown');
-	});
-
-	it('should be ok with getRecent error', function() {
-		app.projects.get = sinon.stub().returns({});
-		app.builds.getRecent = sinon.stub().callsArgWith(1, new Error());
-		handler(getReq({projectName: 'ok'}), res);
-
-		checkSvg('unknown');
-	});
-
-	it('should be ok with getRecent non-Error instance error', function() {
-		app.projects.get = sinon.stub().returns({});
-		app.builds.getRecent = sinon.stub().callsArgWith(1, 'error');
-		handler(getReq({projectName: 'ok'}), res);
-
-		checkSvg('unknown');
-	});
-
-	_({
-		done: 'passing',
-		'in-progress': 'in progress',
-		error: 'failing'
-	}).each(function(text, status) {
-		it('should be ok with `' + status + '` build', function() {
+		it('should be ok with project without builds', function() {
 			app.projects.get = sinon.stub().returns({});
-			app.builds.getRecent = sinon.stub()
-				.callsArgWith(1, null, [{status: status}]);
-			handler(getReq({projectName: 'ok'}), res);
+			app.builds.getRecent = sinon.stub().callsArgWith(1, null, []);
+			projectHandler(getReq({projectName: 'ok'}), res);
 
-			checkSvg(text);
+			checkSvg('unknown');
+		});
+
+		it('should be ok with getRecent error', function() {
+			app.projects.get = sinon.stub().returns({});
+			app.builds.getRecent = sinon.stub().callsArgWith(1, new Error());
+			projectHandler(getReq({projectName: 'ok'}), res);
+
+			checkSvg('unknown');
+		});
+
+		it('should be ok with getRecent non-Error instance error', function() {
+			app.projects.get = sinon.stub().returns({});
+			app.builds.getRecent = sinon.stub().callsArgWith(1, 'error');
+			projectHandler(getReq({projectName: 'ok'}), res);
+
+			checkSvg('unknown');
+		});
+
+		_(statusesHash).each(function(text, status) {
+			it('should be ok with `' + status + '` build', function() {
+				app.projects.get = sinon.stub().returns({});
+				app.builds.getRecent = sinon.stub()
+					.callsArgWith(1, null, [{status: status}]);
+				projectHandler(getReq({projectName: 'ok'}), res);
+
+				checkSvg(text);
+			});
+		});
+	})
+
+	describe('build', function() {
+		it('should be ok with unexisting build', function() {
+			app.builds.get = sinon.stub().callsArgWith(1, null, null);
+			buildHandler(getReq({id: 777}), res);
+
+			expect(res.sendStatus.firstCall.args[0]).to.eql(404);
+		});
+
+		it('should be ok with get error', function() {
+			app.builds.get = sinon.stub().callsArgWith(1, new Error());
+			buildHandler(getReq({id: 1}), res);
+
+			expect(res.sendStatus.firstCall.args[0]).to.eql(404);
+		});
+
+		it('should be ok with getRecent non-Error instance error', function() {
+			app.builds.get = sinon.stub().callsArgWith(1, 'error');
+			buildHandler(getReq({id: 1}), res);
+
+			expect(res.sendStatus.firstCall.args[0]).to.eql(404);
+		});
+
+		_(statusesHash).each(function(text, status) {
+			it('should be ok with `' + status + '` build', function() {
+				app.builds.get = sinon.stub()
+					.callsArgWith(1, null, {status: status});
+				buildHandler(getReq({id: 1}), res);
+
+				checkSvg(text);
+			});
 		});
 	});
 });
